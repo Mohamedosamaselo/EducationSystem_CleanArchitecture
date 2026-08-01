a982e3a11cb3824a42f546b5349032b43cef98e6
Mohamedosamaselo <mo5195278@gmail.com>
2026-08-01 19:26:37 +0300

    BaseEntity , BaseAuditableEntity

---
+++ EducationSystem.Domain/Common/BaseAuditableEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace EducationSystem.Domain.Common;
+
+public class BaseAuditableEntity : BaseEntity
+{
+    public DateTime CreatedAt { get; set; }
+
+    public Guid? CreatedBy { get; set; }
+
+    public DateTime? ModifiedAt { get; set; }
+
+    public Guid? LastModifiedBy { get; set; }
+}
