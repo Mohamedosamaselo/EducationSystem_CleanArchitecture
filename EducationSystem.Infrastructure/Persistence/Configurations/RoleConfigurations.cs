a823f38374b73576fb636266a37616c162a0a084
Mohamedosamaselo <mo5195278@gmail.com>
2026-08-02 14:27:10 +0300

    RoleConfigurations

---
+++ EducationSystem.Infrastructure/Persistence/Configurations/RoleConfigurations.cs
@@ -0,0 +1,21 @@
+﻿using EducationSystem.Domain.Entities;
+using EducationSystem.Infrastructure.Persistence.Configurations.Common;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EducationSystem.Infrastructure.Persistence.Configurations;
+
+public class RoleConfigurations : BaseAuditableEntityConfiguration<Role>
+{
+    public override void Configure(EntityTypeBuilder<Role> builder)
+    {
+        base.Configure(builder);
+
+        builder.Property(u => u.Name)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(u => u.Description)
+           .IsRequired()
+           .HasMaxLength(100);
+    }
+}
