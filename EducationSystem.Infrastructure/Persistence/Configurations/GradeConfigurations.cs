63420718327999d211bbbcf6c943a55df7fef99d
Mohamedosamaselo <mo5195278@gmail.com>
2026-08-02 12:58:22 +0300

    GradeConfigurations

---
+++ EducationSystem.Infrastructure/Persistence/Configurations/GradeConfigurations.cs
@@ -0,0 +1,22 @@
+﻿using EducationSystem.Domain.Entities;
+using EducationSystem.Infrastructure.Persistence.Configurations.Common;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EducationSystem.Infrastructure.Persistence.Configurations;
+
+public class GradeConfigurations : BaseAuditableEntityConfiguration<Grade>
+{
+    public override void Configure(EntityTypeBuilder<Grade> builder)
+    {
+        base.Configure(builder);
+
+        builder.Property(x => x.Name)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.HasOne(x => x.School)
+            .WithMany(x => x.Grades)
+            .HasForeignKey(x => x.SchoolId)
+            .OnDelete(Microsoft.EntityFrameworkCore.DeleteBehavior.Cascade);
+    }
+}
