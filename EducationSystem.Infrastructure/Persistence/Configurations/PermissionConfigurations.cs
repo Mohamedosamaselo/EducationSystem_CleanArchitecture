37147f2724c361554479ef05bd0f435143313bd9
Mohamedosamaselo <mo5195278@gmail.com>
2026-08-02 14:35:47 +0300

    PermissionConfigurations

---
+++ EducationSystem.Infrastructure/Persistence/Configurations/PermissionConfigurations.cs
@@ -0,0 +1,27 @@
+﻿using EducationSystem.Domain.Entities;
+using EducationSystem.Infrastructure.Persistence.Configurations.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EducationSystem.Infrastructure.Persistence.Configurations;
+
+internal class PermissionConfigurations : BaseAuditableEntityConfiguration<Permission>
+{
+    public override void Configure(EntityTypeBuilder<Permission> builder)
+    {
+        base.Configure(builder);
+
+        builder.Property(p => p.Name)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(p => p.Description)
+           .IsRequired()
+           .HasMaxLength(100);
+
+        builder.HasOne(p => p.Role)
+            .WithMany(p => p.Permissions)
+            .HasForeignKey(p => p.RoleId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
