f11704d82127084b298925eb37c9d317372efaf7
Mohamedosamaselo <mo5195278@gmail.com>
2026-08-02 12:45:28 +0300

    SubjectCofigurations

---
+++ EducationSystem.Infrastructure/Persistence/Configurations/SubjectConfigurations.cs
@@ -0,0 +1,26 @@
+﻿using EducationSystem.Domain.Entities;
+using EducationSystem.Infrastructure.Persistence.Configurations.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EducationSystem.Infrastructure.Persistence.Configurations;
+
+public class SubjectConfigurations : BaseAuditableEntityConfiguration<Subject>
+{
+    public override void Configure(EntityTypeBuilder<Subject> builder)
+    {
+        base.Configure(builder);
+
+        builder.Property(e => e.Name)
+               .IsRequired()
+               .HasMaxLength(100);
+
+        builder.Property(e => e.Code)
+            .IsRequired();
+
+        builder.HasOne(s => s.School)
+               .WithMany(s => s.Subjects)
+               .HasForeignKey(s => s.SchoolId)
+               .OnDelete(DeleteBehavior.Cascade);
+    }
+}
